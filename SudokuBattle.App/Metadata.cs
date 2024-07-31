0788ee20a2aeefb57e89e46ec9e1bd9e07063f20
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-07-31 18:29:39 +0200

    add minver and display version

---
+++ SudokuBattle.App/Metadata.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace SudokuBattle.App;
+
+public static class Metadata
+{
+    public static string? Version { get; private set; }
+
+    static Metadata()
+    {
+        Assembly assembly = typeof(Metadata).Assembly;
+        Version = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+    }
+}
