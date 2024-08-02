2453ed5ad237dbf52c684ba73a5d52e4883f4fb1
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-02 12:57:16 +0200

    add test that fails

---
+++ Tests.Sudoku/TEST_TO_REMOVE.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace Tests.Sudoku;
+
+[TestClass]
+public class TEST_TO_REMOVE
+{
+    [TestMethod]
+    public void FAILS()
+    {
+        true.Should().BeFalse();
+    }
+}
