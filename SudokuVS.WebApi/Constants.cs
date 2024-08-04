e04a5cf73cd112144fd3e1a003bac246dbfde622
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-04 13:55:37 +0200

    add github links

---
+++ SudokuVS.WebApi/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace SudokuVS.WebApi;
+
+static class Constants
+{
+    public const string ProductName = "SudokuVS";
+    public const string GithubRepository = "https://github.com/SudokuVS/SudokuVS";
+}
