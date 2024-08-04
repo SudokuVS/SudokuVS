1294b686b67b577a43cbb5f90461b0befad48c6c
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-04 21:26:11 +0200

    update game ui

--- SudokuVS.Game/Persistence/ISudokuGamesRepository.cs
+++ SudokuVS.Game/Persistence/ISudokuGamesRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace SudokuVS.Game.Persistence;
+﻿using SudokuVS.Game.Exceptions;
+
+namespace SudokuVS.Game.Persistence;
 
 public interface ISudokuGamesRepository
 {
@@ -7,4 +9,10 @@ public interface ISudokuGamesRepository
     Task<SudokuGame?> GetAsync(Guid id, CancellationToken cancellationToken = default);
     Task SaveAsync(SudokuGame game, CancellationToken cancellationToken = default);
     Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
+
+public static class SudokuGamesRepositoryExtensions
+{
+    public static async Task<SudokuGame> RequireAsync(this ISudokuGamesRepository repository, Guid id, CancellationToken cancellationToken = default) =>
+        await repository.GetAsync(id, cancellationToken) ?? throw new EntityNotFoundException<SudokuGame>(id);
 }
--- SudokuVS.WebApi/Services/GameplayService.cs
+++ SudokuVS.WebApi/Services/GameplayService.cs
@@ -15,7 +15,7 @@ public class GameplayService
         _repository = repository;
     }
 
-    public async Task<SudokuGame> CreateGameAsync(string? gameName, SudokuGameOptions gameOptions, UserIdentity creatorUser)
+    public async Task<SudokuGame> CreateGameAsync(string? gameName, SudokuGameOptions gameOptions, UserIdentity creatorUser, CancellationToken cancellationToken = default)
     {
         SudokuGame? game = SudokuGame.Create(gameName, gameOptions);
         if (game == null)
@@ -23,9 +23,27 @@ public class GameplayService
             throw new InternalErrorException("Failed to create game.");
         }
 
-        PlayerState playerState = game.Join(creatorUser, PlayerSide.Player1);
-        await _repository.SaveAsync(game);
+        game.Join(creatorUser, PlayerSide.Player1);
+        await _repository.SaveAsync(game, cancellationToken);
 
         return game;
+    }
+
+    public async Task JoinGameAsync(Guid gameId, UserIdentity user, CancellationToken cancellationToken = default)
+    {
+        SudokuGame game = await _repository.RequireAsync(gameId, cancellationToken);
+
+        if (game.Player1 == null)
+        {
+            game.Join(user, PlayerSide.Player1);
+        }
+        else if (game.Player2 == null)
+        {
+            game.Join(user, PlayerSide.Player2);
+        }
+        else
+        {
+            throw new InvalidOperationException("Game full");
+        }
     }
 }
