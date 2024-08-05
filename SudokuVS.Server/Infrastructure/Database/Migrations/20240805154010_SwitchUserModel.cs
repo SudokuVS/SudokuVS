27db1edd153492caa7c24bedae92043da460c911
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-05 17:41:04 +0200

    add migration

---
+++ SudokuVS.Server/Infrastructure/Database/Migrations/20240805154010_SwitchUserModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SudokuVS.Server.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SwitchUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+        }
+    }
+}
