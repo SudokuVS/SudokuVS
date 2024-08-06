cb1640445a5344424d3a9828201b1d64e32b5f0f
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-06 12:26:11 +0200

    rename class

--- SudokuVS.Game/Utils/QueryableExtensions.cs
+++ SudokuVS.Game/Utils/QueryableExtensions.cs
@@ -4,7 +4,7 @@ using SudokuVS.Game.Exceptions;
 
 namespace SudokuVS.Game.Utils;
 
-public static class EntityFrameworkExtensions
+public static class QueryableExtensions
 {
     public static async Task<TEntity> RequireAsync<TEntity>(this DbSet<TEntity> set, object key, CancellationToken cancellationToken = default) where TEntity: class =>
         await set.FindAsync([key], cancellationToken) ?? throw new EntityNotFoundException<TEntity>(key);
