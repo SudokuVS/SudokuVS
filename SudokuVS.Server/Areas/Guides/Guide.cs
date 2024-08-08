6aed8484c3b3ecf1a0cda5e9c211d9b414fab035
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-08-09 00:16:31 +0200

    improve index

---
+++ SudokuVS.Server/Areas/Guides/Guide.cs
@@ -0,0 +1,9 @@
+﻿namespace SudokuVS.Server.Areas.Guides;
+
+public class Guide
+{
+    public required string Page { get; set; }
+    public required string Title { get; set; }
+    public required string Summary { get; set; }
+    public bool Interactive { get; set; }
+}
